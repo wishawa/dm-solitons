81d54570026e61d0aa4086ce5714e5f7f1b27406
Wisha Wanichwecharungruang <ww39@rice.edu>
2022-06-23 23:48:14 -0500

    make backup of Psi before doing the vector SI step. this fixes the mass conservation.

--- helpers/halfKick.m
+++ helpers/halfKick.m
@@ -11,14 +11,16 @@ for j = 1:3
 	NewPsi{j} = exp(-1i * (dt / 4) * VScalar) .* NewPsi{j};
 end
 
-aVal = -1i * (dt / 2) * simConsts.siCoef;
-MCoef = (exp(aVal * Rho) - 1) ./ Rho;
+BPsi = NewPsi;
+
+rhoMul = -1i * (dt / 2) * simConsts.siCoef;
+MCoef = (exp(Rho * rhoMul) - 1) ./ Rho;
 N = simConsts.N;
 for j = 1:3
 	buffer = zeros(N, N, N);
 	for k = 1:3
 		delta = double(j == k);
-		buffer = buffer + (delta + MCoef .* conj(PsiForVsi{j}) .* PsiForVsi{k}) .* NewPsi{k};
+		buffer = buffer + (delta + MCoef .* conj(PsiForVsi{j}) .* PsiForVsi{k}) .* BPsi{k};
 	end
 	NewPsi{j} = buffer;
 end
