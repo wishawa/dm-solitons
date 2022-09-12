9870cd65da6e9f54e40090ac70e5270f2d4240e0
Wisha Wanichwecharungruang <ww39@rice.edu>
2022-09-12 03:31:10 +0000

    cleanup vector kick 0 treshold

--- helpers/stepKickVector.m
+++ helpers/stepKickVector.m
@@ -43,7 +43,7 @@ function Psi = stepKickVector(Psi, dt, simConfig)
 	% 	Psi = kickCorrectionTerm(Psi, OrigPsi, 1, dt / 2, simConfig);
 	% end
 end
-function EigVec = evecFromLambda(A11, A12, A13, A22, A23, A33, lambda, backupPosition)
+function EigVec = evecFromLambda(A11, A12, A13, A22, A23, A33, lambda)
 	M11 = A11 - lambda;
 	M22 = A22 - lambda;
 	M33 = A33 - lambda;
@@ -150,9 +150,9 @@ function NewPsi = kickCorrectionNew(InputPsi, Psi, dt, simConfig)
 	eD22 = exp(1i * ccoef * lambda2);
 	eD33 = exp(1i * ccoef * lambda3);
 
-	Sx1 = evecFromLambda(A11, A12, A13, A22, A23, A33, lambda1, 1);
-	Sx2 = evecFromLambda(A11, A12, A13, A22, A23, A33, lambda2, 2);
-	Sx3 = evecFromLambda(A11, A12, A13, A22, A23, A33, lambda3, 3);
+	Sx1 = evecFromLambda(A11, A12, A13, A22, A23, A33, lambda1);
+	Sx2 = evecFromLambda(A11, A12, A13, A22, A23, A33, lambda2);
+	Sx3 = evecFromLambda(A11, A12, A13, A22, A23, A33, lambda3);
 
 	% Sx = {Sx1, Sx2, Sx3};
 	% lambda = {lambda1, lambda2, lambda3};
@@ -172,7 +172,7 @@ function NewPsi = kickCorrectionNew(InputPsi, Psi, dt, simConfig)
 		reshape(NewPsi(:, 2), [N, N, N]),...
 		reshape(NewPsi(:, 3), [N, N, N]),...
 	};
-	zp = (abs(lambda1) < 1E-12);
+	zp = (abs(lambda1) < 1E-20);
 	for j = 1:3
 		NewPsi{j}(zp) = InputPsi{j}(zp);
 	end
@@ -195,7 +195,7 @@ function NewPsi = kickCorrectionTerm(Psi, PsiCc, sgn, dt, simConfig)
 	PsiCcSqAbsSq = PsiCcSqAbs .^ 2;
 	NewPsi = Psi;
 	MCoef = (exp(sgn * dt^2 * (simConfig.lambda^2 / 32.) * PsiCcSqAbsSq) - 1) ./ PsiCcSq;
-	MCoef(PsiCcSqAbs < 1E-22) = 1;
+	MCoef(PsiCcSqAbs < 1E-20) = 1;
 	% MCoef(isnan(MCoef)) = 0;
 	MCoef = MCoef .* dotVectorArray(PsiCc, Psi);
 	for j = 1:3
@@ -211,7 +211,7 @@ function NewPsi = kickMainTerm(Psi, PsiForOp, dt, simConfig)
 	rhoMul = -0.25i * dt * simConfig.lambda;
 	Rho = getRho(PsiForOp);
 	MCoef = (exp(Rho * rhoMul) - 1) ./ Rho;
-	MCoef(Rho < 1E-22) = 1.;
+	MCoef(Rho < 1E-20) = 1.;
 	NewPsi = Psi;
 	MCoef = MCoef .* dotVectorArray(PsiForOp, Psi);
 	for j = 1:3
